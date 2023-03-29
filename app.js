2c2c7b97a6d17166acdbf678b56f07769e9ee2dd
Md-shefat-masum <mshefat924@gmail.com>
2023-03-29 22:38:10 +0600

    server setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('ok');
+const express = require('express');
+const server = express();
+const port = 5000;
+
+server.get('/',(req, res)=>{
+    res.send('hellow from express');
+})
+
+server.listen(port, ()=>{
+    console.log(`server is running on http://127.0.0.1:${port}`);
+})
